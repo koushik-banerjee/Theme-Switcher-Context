900cc1beca5745e8a2f6ea858fe13aba57e58b4e
Koushik <banerjeekoushik04@gmail.com>
2025-04-13 15:30:55 +0530

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/your-repo-name/',
+  base: '/Theme-Switcher-Context/',
   plugins: [react()],
 })
